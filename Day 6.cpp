8d1701778bb1f6c599965048c1755e7f80c68f5e
Arnav Tiwari <106868854+ArnavO3@users.noreply.github.com>
2023-06-21 12:04:12 +0530

    Create Day 6.cpp

---
+++ Day 6.cpp
@@ -0,0 +1,22 @@
+// Question link : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return 
+//the number of unique elements in nums.
+//Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+//Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of 
+//nums.
+//Return k.
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int left = 0;
+        for(int right =1; right< nums.size(); right++){
+           if(nums[left] != nums[right])
+               left++;
+               nums[left] = nums[right];
+           }
+    return left+1;
+    }
+};
