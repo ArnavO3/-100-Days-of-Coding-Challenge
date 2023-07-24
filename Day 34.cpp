f944eb991f5aa41a8f1029d6a0bbeccc21fcb30b
Arnav Tiwari <106868854+ArnavO3@users.noreply.github.com>
2023-07-24 21:50:24 +0530

    Create Day 34.cpp

---
+++ Day 34.cpp
@@ -0,0 +1,25 @@
+// Question link: https://leetcode.com/problems/powx-n/description/
+//Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        long long N = n;
+        if(N < 0) N = -N;
+        while(N>0){
+            if(N%2==1){
+                ans = ans * x;
+                N--;
+            }
+            else{
+                x = x*x;
+                N/=2;
+            }
+        }
+        if(n < 0){
+            ans = 1.0/ans;
+        }
+        return ans;
+    }
+};
