eaa24304127c199c42b6704b2a27a619edcbad3f
Arnav Tiwari <106868854+ArnavO3@users.noreply.github.com>
2023-09-12 16:02:14 +0530

    Create Day 64.cpp

---
+++ Day 64.cpp
@@ -0,0 +1,24 @@
+// Question link: https://leetcode.com/problems/group-anagrams/description/
+
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order. An Anagram is a word or phrase formed by rearranging the 
+// letters of a different word or phrase, typically using all the original letters exactly once.
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        
+        for(auto x: strs){
+            string word = x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto x: mp){
+            ans.push_back(x.second);
+        }
+        
+        return ans;
+    }
+};
