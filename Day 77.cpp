c56e9df3c992045f985c5b363a71c5cd7b9deffc
Arnav Tiwari <106868854+ArnavO3@users.noreply.github.com>
2023-10-10 17:52:47 +0530

    Create Day 77.cpp

---
+++ Day 77.cpp
@@ -0,0 +1,20 @@
+// Question link: https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/?envType=daily-question&envId=2023-10-02
+
+// There are n pieces arranged in a line, and each piece is colored either by 'A' or by 'B'. You are given a string colors of length n where colors[i] is the color 
+//of the ith piece. Alice and Bob are playing a game where they take alternating turns removing pieces from the line. In this game, Alice moves first.
+// Alice is only allowed to remove a piece colored 'A' if both its neighbors are also colored 'A'. She is not allowed to remove pieces that are colored 'B'.
+// Bob is only allowed to remove a piece colored 'B' if both its neighbors are also colored 'B'. He is not allowed to remove pieces that are colored 'A'.
+// Alice and Bob cannot remove pieces from the edge of the line. If a player cannot make a move on their turn, that player loses and the other player wins.
+// Assuming Alice and Bob play optimally, return true if Alice wins, or return false if Bob wins.
+
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int a=0,b=0;
+        for(int i=1; i<colors.size()-1; i++){
+            if(colors[i-1] == 'A' && colors[i] == 'A' && colors[i+1] == 'A' ) a++;
+            else if(colors[i-1] == 'B' && colors[i] == 'B' && colors[i+1] == 'B' ) b++;
+        }
+        return a>b;
+    }
+};
