f83ba797e6e334b184ca823cb7bbeac239b6fc4d
Arnav Tiwari <106868854+ArnavO3@users.noreply.github.com>
2023-10-15 19:10:59 +0530

    Create Day 84.cpp

---
+++ Day 84.cpp
@@ -0,0 +1,24 @@
+// Question link: https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/
+
+// You are given a positive integer array skill of even length n where skill[i] denotes the skill of the ith player. Divide the players into n / 2 teams of size 2 
+// such that the total skill of each team is equal. The chemistry of a team is equal to the product of the skills of the players on that team.
+// Return the sum of the chemistry of all the teams, or return -1 if there is no way to divide the players into teams such that the total skill of each team is equal.
+
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        sort(skill.begin(),skill.end());
+        long long ans=0;
+        int start=0;
+        int end=skill.size()-1;
+        long sum=skill[start]+skill[end];
+        while(start<=end){
+            long long currsum=skill[start]+skill[end];
+            if(sum!=currsum) return -1;
+            ans=ans+(skill[start]*skill[end]);
+            start++;
+            end--;
+        }
+        return ans;
+    }
+};
